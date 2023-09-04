049993feb057f63c0721f54753e09270317cef09
Dipec001 <dpecchukwu@gmail.com>
2023-09-04 21:06:05 +0100

    config loader

---
+++ static/style.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Remove the loader when the page is fully loaded
+    var loader = document.querySelector(".loader");
+    loader.style.display = "none";
+});
